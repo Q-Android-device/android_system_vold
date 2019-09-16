1ff6314d37284bf85d382fc37eab43c26820babc
Pierre-Hugues Husson <phh@phh.me>
2019-09-16 13:49:05 +0200

    Check needsCheckpoint only if checkpoint is supported
    This is needed because some devices (Xiaomi MiPad 4, uncertified)
    declares a bootctrl HAL in manifest, but doesn't have it.
    vold will then hang in needsCheckpoint waiting for bootctrl
    
    Change-Id: I2dafcbca7e994d7a3ac36ef3698590db2ab482fa

--- cryptfs.cpp
+++ cryptfs.cpp
@@ -1963,9 +1963,11 @@ static int cryptfs_restart_internal(int restart_main) {
             SLOGE("Failed to setexeccon");
             return -1;
         }
-        bool needs_cp = android::vold::cp_needsCheckpoint();
+        bool supportsCheckpoint = false;
+        android::vold::cp_supportsCheckpoint(supportsCheckpoint);
+        bool needs_cp = supportsCheckpoint && android::vold::cp_needsCheckpoint();
 #ifdef CONFIG_HW_DISK_ENCRYPTION
-        while ((mount_rc = fs_mgr_do_mount(&fstab_default, DATA_MNT_POINT, blkdev.data(), 0,
+        while ((mount_rc = fs_mgr_do_mount(&fstab_default, DATA_MNT_POINT, crypto_blkdev, 0,
                                            needs_cp, false)) != 0) {
 #else
         while ((mount_rc = fs_mgr_do_mount(&fstab_default, DATA_MNT_POINT, crypto_blkdev.data(), 0,
